495bc502ca6b001eef0b2630235f1486a7d76738
vagrant <vagrant@vagrant.vm>
2017-06-27 23:09:44 +0000

    Media files added

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -86,7 +86,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
-
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = (STATIC_PATH, )
+
+# Media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
