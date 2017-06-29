9c21952c781853b527363a0396582c043de9f13e
vagrant <vagrant@vagrant.vm>
2017-06-29 21:42:17 +0000

    Initial model and database created

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -63,8 +63,8 @@ WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rango',
     }
 }
 
