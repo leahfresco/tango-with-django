a13cfd535600fa50e3519df1b6979e1024a43dcd
LeahYukelson <leahyukelson@gmail.com>
2017-07-03 00:03:28 +0000

    Creating login functionality

--- rango/urls.py
+++ rango/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
         url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
         url(r'^category/(?P<category_name_slug>[\w\-]+)/add-page/$', views.add_page, name='add_page'),
         url(r'^register/$', views.register, name='register'),
+        url(r'^login/$', views.login, name='login'),
         )
 
