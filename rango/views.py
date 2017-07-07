9ead1882419f7f6f8dff33fab5de6a54b88031be
LeahYukelson <leahyukelson@gmail.com>
2017-07-07 02:27:14 +0000

    Tracking page click throughs

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
@@ -60,7 +60,7 @@ def category(request, category_name_slug):
 
         # Retrieve all of the associated pages.
         # Note that filter returns >= 1 model instance.
-        pages = Page.objects.filter(category=category)
+        pages = Page.objects.filter(category=category).order_by('-views')
 
         # Adds our results list to the template context under name pages.
         context_dict['pages'] = pages
@@ -227,7 +227,6 @@ def user_login(request):
 def restricted(request):
     return render(request, 'rango/restricted.html')
 
-# Use the login_required() decorator to ensure only those logged in can access the view.
 @login_required
 def user_logout(request):
     # Since we know the user is logged in, we can now just log them out.
@@ -235,3 +234,19 @@ def user_logout(request):
 
     # Take the user back to the homepage.
     return HttpResponseRedirect('/rango/')
+
+def track_url(request):
+    page_id = None
+    url = '/rango/'
+    if request.method == 'GET':
+        if 'page_id' in request.GET:
+            page_id = request.GET['page_id']
+            try:
+                page = Page.objects.get(id=page_id)
+                page.views = page.views + 1
+                page.save()
+                url = page.url
+            except:
+                pass
+
+    return redirect(url)
